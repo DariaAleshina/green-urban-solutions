048f608ebcf908dcde526a48b3a634e822abe544
Daria Aleshina <153553065+DariaAleshina@users.noreply.github.com>
2025-03-14 18:04:50 +0100

    1 testimonial corousel finish design add js animation (#2)
    * added new pics, optimised
    
    * added dynamic dot container creation
    
    * animation added

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const section2 = document.querySelector('.section--2');
 const containerFAQ = document.querySelector('.accordion-container');
 const itemsFAQ = document.querySelectorAll('.accordion-item');
 
+const corouselContainer = document.querySelector('.corousel-container');
+const testimonials = document.querySelectorAll('.corousel-testimonial-text');
+const testimonialImages = document.querySelectorAll('.corousel-testimonial-img');
+const dotContainer = document.querySelector('.corousel-dots');
+const btnTestimonialsLeft = document.querySelector('.btn-corousel--left');
+const btnTestimonialsRight = document.querySelector('.btn-corousel--right');
+
+// INIT SETUP
+let currentTestimonial = 0;
+const maxNumberTestimonials = testimonials.length;
 
 // sections reveal while scrolling
 const revealSection = function (entries, observer) {
@@ -53,4 +63,73 @@ containerFAQ.addEventListener('click', function (event) {
     if (!openStatus) clickedItemFAQ.classList.add('active');
 });
 
+// COROUSEL
+// Corousel functions
 
+const createDots = function () {
+    testimonials.forEach(function (_, i) {
+        dotContainer.insertAdjacentHTML(
+            'beforeend',
+            `<button class="btn-dot" data-number=${i}>&nbsp;</button>`
+        );
+    });
+}
+
+
+const activateDot = function (dotNumber) {
+    const dots = document.querySelectorAll('.btn-dot');
+    dots.forEach(dot => {
+        dot.classList.remove('dot-active');
+        +dot.dataset.number === dotNumber && dot.classList.add('dot-active')
+    });
+}
+
+const openTestimonial = function (testimonialNumber) {
+    testimonials.forEach((el, i) => {
+        el.classList.add('hidden');
+        if (i === testimonialNumber) el.classList.remove('hidden');
+    })
+
+    testimonialImages.forEach((img, i) => {
+        img.classList.add('hidden');
+        if (i === testimonialNumber) img.classList.remove('hidden');
+    })
+}
+
+const openNextTestimonial = function () {
+    if (currentTestimonial === maxNumberTestimonials - 1) {
+        currentTestimonial = 0;
+    } else {
+        currentTestimonial++;
+    }
+    openTestimonial(currentTestimonial);
+    activateDot(currentTestimonial);
+}
+
+const openPrevTestimonial = function () {
+    if (currentTestimonial === 0) {
+        currentTestimonial = maxNumberTestimonials - 1;
+    } else {
+        currentTestimonial--;
+    }
+    openTestimonial(currentTestimonial);
+    activateDot(currentTestimonial);
+}
+
+// Corousel initial set up
+createDots();
+activateDot(currentTestimonial);
+openTestimonial(currentTestimonial);
+
+// Corousel - buttons click handling
+
+dotContainer.addEventListener('click', function (event) {
+    const clickedDot = event.target.closest('.btn-dot');
+    if (!clickedDot) return;
+    const clickedDotNumber = +clickedDot.dataset.number;
+    openTestimonial(clickedDotNumber);
+    activateDot(clickedDotNumber);
+});
+
+btnTestimonialsLeft.addEventListener('click', openPrevTestimonial);
+btnTestimonialsRight.addEventListener('click', openNextTestimonial);
